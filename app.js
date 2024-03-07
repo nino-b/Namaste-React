305bce4cb4eb1659c90599aa888b943c204b63cf
Nino Burtchuladze <burtchuladze.dev@gmail.com>
2024-03-07 22:33:04 +0400

    04 added data

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import hamburgerIcon from '/assets/img/hamburger-svgrepo-com.svg';
-import adjaruli from '/assets/img/Adjaruli_Khachapuri.jpg';
-import star from '/assets/img/star.svg';
+
 
 const Header = () => {
     return (
@@ -26,32 +24,110 @@ const styleCard = {
     backgroundColor: '#f0f0f0'
 }
 
-const Restaurantcard = () => {
+const Restaurantcard = (props) => {
+    const {resData} = props;
     return (
         <div className="res-card" style={styleCard}>
             <img 
             className="res-logo"
-            src={adjaruli} 
+            src={resData.image} 
             alt="Restaurant logo"/>
-            <h3 className="res-name">Adjaruli</h3>
+            <h3 className="res-name">{resData.resName}</h3>
+
             <div className="card-detail-container">
-                <h4 className="region">Georgian, Adjarian</h4>
+                <h4 className="region">{resData.region}</h4>
                 <h4 className="rating">
                     <img className="star" src={star}/>
-                    5.0 
+                    {resData.rating}
                 </h4>
-                <h4 className="delivery-time">38 minutes</h4>
+                <h4 className="delivery-time">{resData.deliveryTime}</h4>
             </div>
         </div>
     )
 }
+
+
+const dataList = [
+    {
+        image: "v1709826147/Namaste%20React/Adjaruli_Khachapuri_p2vduc.webp",
+        resName: "Adjara Feast",
+        region: "Georgian, Adjarian",
+        rating: "5.0",
+        deliveryTime: "38 minutes"
+    },
+    {
+        image: "v1709826495/Namaste%20React/Mtiuluri_Khinkali_xh04wy.webp",
+        resName: "Mtiuluri Kitchen",
+        region: "Georgian, Mriuluri",
+        rating: "4.9",
+        deliveryTime: "30 minutes"
+    },
+    {
+        image: "v1709831126/Namaste%20React/Kakheti_Mtsvadi_vk9f0f.webp",
+        resName: "The Kakhetian Table",
+        region: "Georgian, Kakhetian",
+        rating: "5.0",
+        deliveryTime: "50 minutes"
+    },
+    {
+        image: "v1709831125/Namaste%20React/Megruli_Elarji_xthhy2.webp",
+        resName: "Taste of Samegrelo",
+        region: "Georgian, Megruli",
+        rating: "4.9",
+        deliveryTime: "40 minutes"
+    },
+    {
+        image: "v1709831125/Namaste%20React/Guruli_Pkhali_ec4g7j.webp",
+        resName: "Guria's Garden",
+        region: "Georgian, Gurian",
+        rating: "4.9",
+        deliveryTime: "30 minutes"
+    },
+    {
+        image: "v1709831125/Namaste%20React/Imeruli_Sacivi_nx32hj.webp",
+        resName: "Imereti Harvest",
+        region: "Georgian, Imeruli",
+        rating: "4.7",
+        deliveryTime: "30 minutes"
+    },
+    {
+        image: "v1709832590/Namaste%20React/Svaneti_Kubdari_o2pxlh.jpg",
+        resName: "Svaneti Savors",
+        region: "Georgian, Mriuluri",
+        rating: "4.8",
+        deliveryTime: "55 minutes"
+    },
+    {
+        image: "v1709831124/Namaste%20React/Racha_Shkmeruli_fo1qw7.webp",
+        resName: "Rachuli Delights",
+        region: "Georgian, Svanuri",
+        rating: "4.7",
+        deliveryTime: "32 minutes"
+    },
+    {
+        image: "v1709832590/Namaste%20React/Shotis_Puri_mv0xaz.jpg",
+        resName: "Shotis Puri House",
+        region: "Georgian",
+        rating: "4.9",
+        deliveryTime: "20 minutes"
+    },
+    {
+        image: "v1709832590/Namaste%20React/Churchkhela_aiz0i0.webp",
+        resName: "Georgian Sweets Bazaar",
+        region: "Georgian",
+        rating: "5.0",
+        deliveryTime: "25 minutes"
+    }
+]
+
 
 const Body = () => {
     return (
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <Restaurantcard/>
+                <Restaurantcard resData={AdjaraFeastData}/>
+                <Restaurantcard resData={MtiuluriKitchenData}/>
             </div>
         </div>
     )
--- assignments/episode04/notes.md
+++ assignments/episode04/notes.md
@@ -31,3 +31,99 @@ If you want to write inline style in JSX, you should
 - put that argument in curly braces (because it is a JS code).
 
 Properties should be written with camel case (```backgroundColor```).
+
+
+
+
+
+<h1>What is <b>props</b>?</h1>
+
+- <b>props</b> also called <b>properties</b>.
+- Argument to a function.
+- Saying: 'passing a <b>prop</b> to a component' is similar to saying: 'passing an <b>argument</b> to a function".
+- React will wrap those <b>props</b> in an object and pass to the component.
+- When we have to dynamically pass in some data, we use <b>props</b>.
+
+We can <b>destructure props oject on the fly</b>.
+- instead of writing this:
+```js
+const Restaurantcard = (props) => {
+    return (
+        <div>
+            <h3>{props.resName}</h3>
+            <div>
+                <h4>{props.region}</h4>
+                <h4>{props.rating}</h4>
+                <h4>{props.deliveryTime}</h4>
+            </div>
+        </div>
+    )
+}
+
+<Restaurantcard
+    image={adjaruli}
+    resName="Adjara Feast"
+    region="Georgian, Adjarian."
+    rating="5.0"
+    deliveryTime="38 minutes"
+/>
+```
+
+- We can write this way:
+```js
+const Restaurantcard = ({resName, region, rating, deliveryTime}) => {
+    return (
+        <div>
+            <h3>{resName}</h3>
+            <div>
+                <h4>{region}</h4>
+                <h4>{rating}</h4>
+                <h4>{deliveryTime}</h4>
+            </div>
+        </div>
+    )
+}
+
+<Restaurantcard
+    resName="Adjara Feast"
+    region="Georgian, Adjarian."
+    rating="5.0"
+    deliveryTime="38 minutes"
+/>
+```
+
+
+
+
+
+<h1>Config driven UI</h1>
+
+- Approach in designing and developing.
+- Layout, behavior and appearance of UI are determined by <b>configuration files</b> or <b>objects</b> rather than hard-coded values.
+- Configuration files come from backend in a JSON format.
+
+Allows for:
+- Flexible and dynamic UI.
+- Separation of Concerns - decoupling logic from values.
+- Reusability - common UI code can be reused in different sutuations.
+
+
+
+
+
+<h1>Optional chaining</h1>
+
+Allows accessing deeply nested properties on the object without need to check if that property is null or undefined.
+
+1. 
+const city = person?.address?.city;
+
+- If person does not exist, returns undefined.
+- If person exists, checks for address. If address does not exist, returns undefined.
+- If person and address exist, checks for city. If city does not exist, returns undefined, otherwise it returns whatever city is set to (even if it is explicity set to null or undefined, it will return null or undefined).
+
+2. 
+const city = person.address.city;
+
+- If there exists everything, it will return value of city property.
+- If any of those does not exist, or is null/undefined, it will throw a <b>TypeError</b> (saying like: "Cannot read properties of undefined (reading 'address')").
